846713037ab139ff5b8f7ad4d3549408c8497ba4
LeoCodeRu <leo@leocode.ru>
2025-02-27 22:37:37 +0300

    Transfer data from Collection Triple-switch to single Library

--- lib/src/triple_switch.dart
+++ lib/src/triple_switch.dart
@@ -2,91 +2,136 @@
 import 'package:flutter/material.dart';
 
 // Import Modules
-import 'switch_model.dart';
-import 'switch_notifier.dart';
-import 'triple_switch_ui.dart';
+import 'switch_state.dart';
+import 'default_decoration_switch.dart';
 
-enum SwitchPosition {on, wait, off}
+//enum SwitchPosition {on, wait, off}
 
 class TripleSwitch extends StatelessWidget {
-  final String id;
-  final SwitchPosition? value;
-  final bool? result;
-  final int? timeoutOffOn;
-  final int? timeoutOnOff;
-  final ValueChanged<SwitchPosition?>? onChanged;
 
-  const TripleSwitch({super.key, required this.id, this.value, this.result, this.timeoutOffOn, this.timeoutOnOff, this.onChanged});
+  final String id;                              /// Unique ID of switch
+  final bool? enabled;                          /// Enabled or Disabled switch
+  //final bool value;                             /// Default value of switch, true - On / false - Off
+  final int? timeoutOffOn;                      /// Timeout for wait switching Off -> On
+  final int? timeoutOnOff;                      /// Timeout for wait switching On -> Off
+  final Function? functionOffOn;                /// Calling a heavy function during switching Off -> On
+  final Function? functionOnOff;                /// Calling a heavy function during switching On -> Off
+  final List<dynamic>? argumentsOffOn;          /// Arguments of the called heavy function, Off -> On
+  final List<dynamic>? argumentsOnOff;          /// Arguments of the called heavy function, On -> Off
+  //final ValueChanged<bool>? onChanged;          /// Event returns new position of switch, it may be old or new position
 
-  //Get Timeout by dependence from direction Off->On / On->Off
-  int timeoutByDirection(SwitchPosition? swp) =>
-      ((swp == SwitchPosition.on) ? timeoutOnOff : timeoutOffOn) ?? 0;
+  final BoxDecoration? decorationTrackOn;
+  final BoxDecoration? decorationTrackOff;
+  final BoxDecoration? decorationTrackWait;
+  final BoxDecoration? decorationTrackDisabled;
 
-  SwitchPosition? non(SwitchPosition? value) {
-    if (value == null) return null;
-    return (value == SwitchPosition.on) ? SwitchPosition.off : SwitchPosition.on;
-  }
+  final BoxDecoration? decorationSliderOn;
+  final BoxDecoration? decorationSliderOff;
+  final BoxDecoration? decorationSliderWait;
+  final BoxDecoration? decorationSliderDisabled;
+
+  final Size? sizeTrack;
+  final Size? sizeSlider;
+
+  final int?  timeout;
+  final String? textOn;
+  final String? textOff;
+  final String? textWait;
+  final String? textDisabled;
+  final TextStyle? textStyleEnabled;
+  final TextStyle? textStyleDisabled;
+
+  const TripleSwitch({
+    super.key,
+    required this.id,
+    //required this.value,
+    this.enabled,
+    this.timeoutOffOn,
+    this.timeoutOnOff,
+    this.argumentsOffOn,
+    this.functionOffOn,
+    this.functionOnOff,
+    this.argumentsOnOff,
+    //this.onChanged,
+    this.decorationTrackOn,
+    this.decorationTrackOff,
+    this.decorationTrackWait,
+    this.decorationTrackDisabled,
+    this.decorationSliderOn,
+    this.decorationSliderOff,
+    this.decorationSliderWait,
+    this.decorationSliderDisabled,
+    this.sizeTrack,
+    this.sizeSlider,
+    this.timeout,
+    this.textOn,
+    this.textOff,
+    this.textWait,
+    this.textDisabled,
+    this.textStyleEnabled = defaultTextStyleEnabled,
+    this.textStyleDisabled = defaultTextStyleDisabled,
+  });
 
   @override
   Widget build(BuildContext context) {
+    final SwitchState switches = SwitchState();
 
-    //final SwitchState switchers = SwitchState()..data.addAll({id : SwitchModel()});
-    final SwitchState switchers = SwitchState();
-    //print('Keys: ${switchers.data.keys}');
+    BoxDecoration? trackDecor;
+    BoxDecoration? sliderDecor;
+    Alignment? pos;
+    String? text;
+
+    if (switches.data[id]!.timeout != null) {
+      trackDecor = decorationTrackWait ?? defaultDecorationTrackWait;
+      sliderDecor = decorationSliderWait ?? defaultDecorationSliderWait;
+      pos = Alignment.center;
+      text = textWait ?? timeout.toString();
+    } else {
+      if (switches.data[id]!.position) {
+        trackDecor = decorationTrackOn ?? defaultDecorationTrackOn;
+        sliderDecor = decorationSliderOn ?? defaultDecorationSliderOn;
+        pos = Alignment.centerRight;
+        text = textOn;
+      } else  {
+        trackDecor = decorationTrackOff ?? defaultDecorationTrackOff;
+        sliderDecor = decorationSliderOff ?? defaultDecorationSliderOff;
+        pos = Alignment.centerLeft;
+        text = textOff;
+      }
+    }
 
     return ListenableBuilder(
-          listenable: switchers,
-          builder: (context, child) {
-            print('Switch notifier position: ${switchers.data[id]!.position}');
-            print('Switch notifier timout:   ${switchers.data[id]!.timeout}');
-            return TripleSwitchUI(
-              position: (result == null) ? (switchers.data[id]!.position ?? value) : (result! ? non(value) : value),
-              //position: switchers.data[id]!.position,
-              timeout: switchers.data[id]!.timeout == 0 ? timeoutByDirection(switchers.data[id]!.position ?? value) : switchers.data[id]!.timeout,
-              //timeout: switchers.data[id]!.timeout,
-              onChanged: () {
-                if (onChanged != null) {
-                  if (timeoutByDirection(switchers.data[id]!.position ?? value) != 0) {
-                    switchers.setPosition(id, SwitchPosition.wait);
-                    switchers.start(id, timeoutByDirection(switchers.data[id]!.position ?? value));
-                  } else {
-
-                    // Set Switcher to new position
-                    switchers.setPosition(id, (value == SwitchPosition.on) ? SwitchPosition.off : SwitchPosition.on);
-                  }
-                  onChanged!(switchers.data[id]!.position);
-                } else {
-                  onChanged;
-                }
-              },
-
-
-              // onChanged: (onChanged != null) ? (value) {
-              //   if (value == SwitchPosition.wait) {
-              //     switchers.start(id, timeoutByDirection(switchers.data[id]!.position ?? value));
-              //   }
-              //   // Set Switcher to new position
-              //   switchers.setPosition(id, value);
-              //
-              //   // TODO: Запуск Изолята с конкретной операцией вероятно выше по дереву ?!?
-              //   onChanged!(value);
-              // } : null,
-            );
-          }
-      );
+      listenable: switches,
+      builder: (BuildContext ctx, child) {
+        return GestureDetector(
+          onTap: () {
+            if(switches.data[id]!.timeout == null) { // preventing a restart
+              switches.start(id,
+                  switches.data[id]!.position ? timeoutOnOff : timeoutOffOn,
+                  switches.data[id]!.position ? functionOnOff : functionOffOn,
+                  switches.data[id]!.position ? argumentsOnOff : argumentsOffOn
+              );
+            }
+            ///: null; //switches.stop(id); Нет смысла останавливать запрос, т.к. он д/б отработан
+          },
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 200),
+            decoration: (enabled == false) ? defaultDecorationTrackDisabled : trackDecor,
+            alignment: (enabled == false) ? Alignment.center : pos,
+            width: sizeTrack != null ? sizeTrack!.width : 60.0,
+            height: sizeTrack != null ? sizeTrack!.height : 30.0,
+            child: Container(
+              decoration: (enabled == false) ? defaultDecorationSliderDisabled : sliderDecor,
+              alignment: Alignment.center,
+              width:  sizeSlider != null ? sizeSlider!.width : 30.0,
+              height: sizeSlider != null ? sizeSlider!.height : 30.0,
+              child: Text((enabled == false) ? (textDisabled ?? '') : (text ?? ''),
+                style: (enabled == false) ? textStyleDisabled : textStyleEnabled,
+              ),
+            ),
+          ),
+        );
+      }
+    );
   }
 }
-
-// void startTimer(({int timeout, SendPort sendPort}) data) async {
-//   for (int i = (data.timeout - 1); i >= 0; --i) {
-//     await Future.delayed(const Duration(seconds: 1), () => data.sendPort.send(i));
-//   }
-//   data.sendPort.send(null);
-//   //print('isolate finished');
-// }
-
-// Если Таймер еще идет и получен результат с Изолята, если результат положительный
-// Переключаем тумблер дальше, иначе возвращаем в исходную позицию
-// if ((timeOut != null) && (resData.exist)) {
-//   swPos = resData ? iPos(prePos) : prePos;
-// }
--- lib/triple_switch.dart
+++ lib/triple_switch.dart
@@ -1,5 +1,4 @@
 library triple_switch;
 
+export 'src/switch_state.dart';
 export 'src/triple_switch.dart';
-export 'src/switch_notifier.dart';
-export 'src/switch_model.dart';
